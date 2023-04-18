87d3ae93a5b358c6a2f760c972d5a2d04cae6cf9
louispaulet <louispaulet13@gmail.com>
2023-04-18 16:16:45 +0200

    first working version

---
+++ main.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', function() {
+  fetch('article.json')
+    .then(response => response.json())
+    .then(data => {
+      document.getElementById('article-title').innerText = data.title;
+      document.getElementById('article-author').innerText = data.author;
+      document.getElementById('article-date').innerText = data.date;
+      document.getElementById('article-content').innerText = data.content;
+    });
+});
