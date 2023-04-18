3d46c8e8ab5a9ebcad2eec08a3c1219321ea8c33
louispaulet <louispaulet13@gmail.com>
2023-04-18 17:39:23 +0200

    working search bar and dropdown version

--- main.js
+++ main.js
@@ -1,72 +1,80 @@
+let articles = [];
+
 document.addEventListener('DOMContentLoaded', function() {
-  // Initialize the Materialize dropdown menu
-  const dropdowns = document.querySelectorAll('.dropdown-trigger');
-  M.Dropdown.init(dropdowns);
+  if (document.getElementById('article-dropdown')) {
+    // Initialize the Materialize dropdown menu
+    const dropdowns = document.querySelectorAll('.dropdown-trigger');
+    const instances = M.Dropdown.init(dropdowns);
 
-  // Fetch articles from the JSON file
-  fetch('articles.json')
-    .then(response => response.json())
-    .then(articles => {
-      const articleDropdown = document.getElementById('article-dropdown');
+    // Fetch articles from the JSON file
+    fetch('articles.json')
+      .then(response => response.json())
+      .then(loadedArticles => {
+        articles = loadedArticles;
+        const articleDropdown = document.getElementById('article-dropdown');
 
-      articles.forEach(article => {
-        // Create a dropdown item for each article
+        articles.forEach(article => {
+          // Create a dropdown item for each article
+          const listItem = document.createElement('li');
+          const link = document.createElement('a');
+          link.href = `article.html?title=${encodeURIComponent(article.title)}`;
+          link.innerText = article.title;
+          listItem.appendChild(link);
+          articleDropdown.appendChild(listItem);
+        });
+      });
+  }
+
+  if (document.getElementById('search')) {
+    const searchInput = document.getElementById('search');
+    const searchResults = document.getElementById('search-results');
+
+    searchInput.addEventListener('input', event => {
+      const searchTerm = event.target.value.toLowerCase();
+      const filteredArticles = articles.filter(article =>
+        article.title.toLowerCase().includes(searchTerm)
+      );
+
+      // Clear the existing search results
+      searchResults.innerHTML = '';
+
+      // Add filtered articles to the search results
+      filteredArticles.forEach(article => {
         const listItem = document.createElement('li');
         const link = document.createElement('a');
         link.href = `article.html?title=${encodeURIComponent(article.title)}`;
         link.innerText = article.title;
         listItem.appendChild(link);
-        articleDropdown.appendChild(listItem);
+        searchResults.appendChild(listItem);
       });
 
-      // Check if the search input element exists
-      const searchInput = document.getElementById('search');
-      if (searchInput) {
-        // Add event listener for search input
-        searchInput.addEventListener('input', event => {
-          const searchTerm = event.target.value.toLowerCase();
-          const filteredArticles = articles.filter(article =>
-            article.title.toLowerCase().includes(searchTerm)
-          );
-
-          // Clear the existing dropdown items
-          articleDropdown.innerHTML = '';
-
-          // Add filtered articles to the dropdown
-          filteredArticles.forEach(article => {
-            const listItem = document.createElement('li');
-            const link = document.createElement('a');
-            link.href = `article.html?title=${encodeURIComponent(article.title)}`;
-            link.innerText = article.title;
-            listItem.appendChild(link);
-            articleDropdown.appendChild(listItem);
-          });
-        });
-      }
+      // Show or hide the search results
+      searchResults.style.display = searchTerm ? 'block' : 'none';
     });
+  }
 
-  const urlParams = new URLSearchParams(window.location.search);
-  const articleTitle = decodeURIComponent(urlParams.get('title'));
+  if (document.getElementById('article-title')) {
+    // Get the article title from the URL
+    const urlParams = new URLSearchParams(window.location.search);
+    const articleTitle = urlParams.get('title');
 
-  if (articleTitle) {
+    // Fetch articles from the JSON file
     fetch('articles.json')
       .then(response => response.json())
       .then(articles => {
+        // Find the requested article
         const article = articles.find(a => a.title === articleTitle);
-        const articleTitleElement = document.getElementById('article-title');
-          if (article) {
-            if (articleTitleElement) {
-              articleTitleElement.innerText = article.title;
-            }
-            document.getElementById('article-author').innerText = article.author;
-            document.getElementById('article-date').innerText = article.date;
-            document.getElementById('article-content').innerText = article.content;
-            document.getElementById('article-cover-picture').src = article.cover_picture;
-          } else {
-            if (articleTitleElement) {
-              articleTitleElement.innerText = 'Article not found';
-            }
-          }
-        });
+
+        if (article) {
+          // Update the article's content
+          document.getElementById('article-title').innerText = article.title;
+          document.getElementById('article-author').innerText = article.author;
+          document.getElementById('article-date').innerText = article.date;
+          document.getElementById('article-content').innerText = article.content;
+          document.getElementById('article-cover-picture').src = article.cover_picture;
+        } else {
+          document.getElementById('article-title').innerText = 'Article not found';
+        }
+      });
   }
 });
